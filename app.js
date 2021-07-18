af8ae6b65345c8cba657d22f21afee25e678bf47
dittapandey <adityapandey51020@gmail.com>
2021-07-18 10:21:21 +0530

    routes files created

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const HomeURL="/aiwc";
 const adminRoutes = require('./routes/admin.routes');
+const noticeRoutes = require('./routes/notice.routes');
+const announcementRoutes = require('./routes/announcement.routes.js');
+const formRoutes = require('./routes/form.routes');
+const linkRoutes = require('./routes/link.routes');
+
+
+
+app.use(HomeURL+"/admin",adminRoutes);
+app.use(HomeURL+"/admin/notice",noticeRoutes);
+app.use(HomeURL+"/admin/announcement",announcementRoutes);
+app.use(HomeURL+"/admin/uploads",adminUploadsRoutes);
+app.use(HomeURL+"/admin/form",formRoutes);
+app.use(HomeURL+"/admin/link",linkRoutes);
 
 app.use()
