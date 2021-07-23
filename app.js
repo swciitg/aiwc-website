23ce00d4c4fe78ade6411d2430e079bc5402b9bf
unknown <gracemarymatson@gmail.com>
2021-07-23 14:06:26 +0530

    basic template up, no authentication yet

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+const BASE_PATH = process.env.BASE_PATH;
+
+app.set('view engine', 'ejs');
+
 const adminRoutes = require('./routes/admin.routes');
 
-app.use()
+app.use(BASE_PATH + "/admin", adminRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
