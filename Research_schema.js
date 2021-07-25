0034a03813ee46bbb9d2ef5a52010bf7e57499a8
LakshyaMittal <lmittal2002@gmail.com>
2021-07-25 13:38:34 +0530

    Research schema added

---
+++ Research_schema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const ResearchSchema = new mongoose.Schema
+
+({
+  title: { type: String },
+  description: { type: String, required: true },
+   path: { type: String, required: true },
+  priority_number: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("Research", ResearchSchema);
