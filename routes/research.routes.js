ea2cfb9d38e558a463a7543c72f31de43cc1c613
GraceMatson <gracemarymatson@gmail.com>
2021-12-10 23:18:05 +0530

    research backend done

--- routes/research.routes.js
+++ routes/research.routes.js
@@ -5,6 +5,7 @@ const researchController = require('../controllers/research.controller');
 router.get('/', researchController.getResearch);
 router.get('/add', researchController.addResearchForm);
 router.post('/', researchController.addResearch);
+router.post('/find', researchController.findResearch);
 router.get('/:id', researchController.editResearchForm);
 router.put('/:id', researchController.editResearch);
 router.delete('/:id', researchController.deleteResearch);
