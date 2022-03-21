50d6466585b1f4ce93e8080092244a7487caebca
Yashi Natu <75984534+yashinatu@users.noreply.github.com>
2022-03-21 17:38:50 +0530

    bug fix

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 //this is to create the backend for the actual frontend page
 const express = require('express');
+const router = express.Router({ mergeParams: true });
 const userController = require("../controllers/user.controller");
 router.get("/", userController.getHome);
 router.get("/partners", userController.getPartners);
@@ -7,5 +8,4 @@ router.get("/about", userController.getAbout);
 router.get("/partners/:partner_id", userController.getPartner);
 router.get("/partners/:person_id", userController.getPerson);
 router.get("/contactUs", userController.getContactUs);
-const router = express.Router({ mergeParams: true });
 module.exports = router;
